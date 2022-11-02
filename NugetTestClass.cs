3347c6a4f7eb4c848e0f8b4f5717cd3da312ec86
oleg fruntov <fruntovoleg@gmail.com>
2022-11-02 12:17:36 +0200

    Add project files.

---
+++ NugetTestClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NugetTest
+{
+    public class NugetTestClass
+    {
+        public static string TEST = "test data";
+    }
+}
